db21879f6683648563cffa4cda4ac1a8c3d7ef1f
David Ridgley <Arximiro@outlook.com>
2018-04-20 23:19:26 -0400

    formatted text in a couple files, removed unused video files

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.listen(port, () => {
-    console.log(`Server is up on port ${port}`);
+  console.log(`Server is up on port ${port}`);
 });
